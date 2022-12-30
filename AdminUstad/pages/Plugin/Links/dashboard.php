c72199dd50e7ac2fa02e8ec4dfddcdff64d14d9d
imranfcc <121158622+imranfcc@users.noreply.github.com>
2022-12-31 00:51:37 +0500

    update

--- AdminUstad/pages/Plugin/Links/dashboard.php
+++ AdminUstad/pages/Plugin/Links/dashboard.php
@@ -11,7 +11,7 @@
 <!-- Start : Your Page Source -->
 <div class="container">
 	<div class="row">
-	<div class="col">
+	<div class="card">
 <?php
 	$result=\P\Links\lists("0");
 	while($row = $result->fetch_assoc()) {
