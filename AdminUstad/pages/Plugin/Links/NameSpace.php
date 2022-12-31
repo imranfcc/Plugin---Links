86c51fdca83a1fc94d474082237dcbe4ec471f85
imranfcc <121158622+imranfcc@users.noreply.github.com>
2022-12-31 22:37:58 +0500

    up

--- AdminUstad/pages/Plugin/Links/NameSpace.php
+++ AdminUstad/pages/Plugin/Links/NameSpace.php
@@ -3,7 +3,7 @@ namespace P\Links;
 
 function lists($Get){
   global $con; 
-  $sql = "SELECT * FROM `sh_pl_links`  WHERE `Id`='$Get[Id]'"; //
+  $sql = "SELECT * FROM `sh_pl_links`  WHERE `Id`='$Get['Id']'"; //
   $result = $con->query($sql);
   return $result;
 }
