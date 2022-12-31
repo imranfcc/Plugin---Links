0f80a32c5fb355438abe4ac3208833019dc20aa8
imranfcc <121158622+imranfcc@users.noreply.github.com>
2022-12-31 23:28:49 +0500

    sidebar

---
+++ AdminUstad/pages/Plugin/Links/Sidebar.php
@@ -0,0 +1,26 @@
+<!-- Right Modal-Sidebar -->
+<div class="modal rightSidebar " id="sh-Modal-RightSidebar" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+<div class="modal-dialog">
+	<div class="modal-content">
+
+	<!-- Modal Header -->
+	<div class="modal-header">
+		<h4 class="modal-title">Menu</h4>
+		<button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+	</div>
+
+	<!-- Modal body -->
+	<div class="modal-body">
+		<div class="btn-group-vertical rounded-0 btn-group-lg w-100">
+		<a href="test.php" class="btn btn-light text-start" >Subscription</a>
+		</div>
+	</div>
+
+	<!-- Modal footer -->
+	<div class="modal-footer">
+		<button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+	</div>
+
+	</div>
+</div>
+</div><!-- END : Right Modal-Sidebar  -->
