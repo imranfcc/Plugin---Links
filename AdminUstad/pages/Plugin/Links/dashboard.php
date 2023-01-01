fd5f7a01278cee9f027ee2bd74a917b14320daa3
imranfcc <121158622+imranfcc@users.noreply.github.com>
2023-01-01 20:30:20 +0500

    new

--- AdminUstad/pages/Plugin/Links/dashboard.php
+++ AdminUstad/pages/Plugin/Links/dashboard.php
@@ -10,11 +10,12 @@ if(isset($_GET["ShowInId"])){ $ShowInId=$_GET["ShowInId"]; } else {  $ShowInId="
 <?php \template\navbar(array("Btn_Search"=>false,"Btn_BackLink"=>"$AdminFolder/Plugin/$Plugin/dashboard")); ?>
 <?php if(is_file("pages/Plugin/".$Plugin."/Sidebar.php")){ include("pages/Plugin/".$Plugin."/Sidebar.php"); } ?>
 <div class="modal" id="sh-Modal-Ajax"></div>
-<div class="modal" id="sh-Modal-Waiting"><div class="modal-dialog modal-dialog-centered text-center-success"><i class="fas fa-cog fa-spin fa-5x mx-auto"></i></div></div>
+<div class="modal" id="sh-Modal-Waiting"><div class="modal-dialog modal-dialog-centered text-center-"><i class="fas fa-cog fa-spin fa-5x mx-auto"></i></div></div>
 <!-- Start : Your Page Source -->
 <style>
 	.card-hover:hover{
 		background-color:#F0FFF0;
+		.text-success
 	}
 	</style>
 <main class="<?php echo \template\cfg("WebWidth"); ?>">
