dc0509d5572548a64b629247081ee1d7e2ccb015
imranfcc <121158622+imranfcc@users.noreply.github.com>
2023-01-02 23:23:05 +0500

    update

--- AdminUstad/pages/Plugin/Links/Pages/AddLink.php
+++ AdminUstad/pages/Plugin/Links/Pages/AddLink.php
@@ -12,6 +12,39 @@
 
 
 echo Welcome to my Add Link Page
+<form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Status</label>
+    <input name="Status" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Status">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+     Title</label>
+    <input name="Title" class="form-control" id="exampleInputPassword1" placeholder="Title">
+  </div>
+  <div class="form-group">
+     Link</label>
+    <input name="Link" class="form-control" id="exampleInputPassword1" placeholder="Link">
+  </div>
+  <div class="form-group">
+     Target</label>
+    <input name="Target" class="form-control" id="exampleInputPassword1" placeholder="Target">
+  </div>
+  <div class="form-group">
+     InId</label>
+    <input name="InId" class="form-control" id="exampleInputPassword1" placeholder="InId">
+  </div>
+  <div class="form-group">
+     Icon</label>
+    <input name="Icon" class="form-control" id="exampleInputPassword1" placeholder="Icon">
+  </div>
+  <div class="form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
 
 <!-- End : Your Page Source -->
 <?php include("pages/inc/bs4_bottombar.php"); ?>
