dfbe017f122307098f2c93247343b7854a68641b
imranfcc <121158622+imranfcc@users.noreply.github.com>
2023-01-02 23:58:43 +0500

    update

--- AdminUstad/pages/Plugin/Links/Pages/AddLink.php
+++ AdminUstad/pages/Plugin/Links/Pages/AddLink.php
@@ -10,37 +10,41 @@
 <div class="modal" id="sh-Modal-Waiting"><div class="modal-dialog modal-dialog-centered text-center"><i class="fas fa-cog fa-spin fa-5x mx-auto"></i></div></div>
 <!-- Start : Your Page Source -->
 
+<main class="<?php echo \template\cfg("WebWidth"); ?>">
+	<div class="row">
+		<form class="">
+			<div class="form-floating">
+  				<select class="form-select" id="sel1" name="sellist">
+   					<option>Enable</option>
+   					<option>Disable</option>
+  				</select>
+  				<label for="sel1" class="form-abel">Select list (select one):</label>
+			</div>
+			<div class="form-floating mb-3 mt-3">
+      			<input type="text" class="form-control" id="email" placeholder="Enter email" name="email">
+      			<label for="email">Title</label>
+			</div>
+			<div class="form-floating mb-3 mt-3">
+      			<input type="text" class="form-control" id="email" placeholder="Enter email" name="email">
+      			<label for="email">Link</label>
+   			</div>
+			<div class="form-floating mb-3 mt-3">
+      			<input type="text" class="form-control" id="email" placeholder="Enter email" name="email">
+      			<label for="email">Target</label>
+   			</div>
+			<div class="form-floating mb-3 mt-3">
+      			<input type="text" class="form-control" id="email" placeholder="Enter email" name="email">
+      			<label for="email">Email</label>
+    		</div>
+			<div class="form-floating mb-3 mt-3">
+      			<input type="text" class="form-control" id="email" placeholder="Enter email" name="email">
+      			<label for="email">Icon</label>
+    		</div>
+  			<button type="submit" class="btn btn-primary">Submit</button> 
+		</form>
+	</div>
+</main>
 
-<form class="">
-<div class="form-floating">
-  <select class="form-select" id="sel1" name="sellist">
-    <option>Enable</option>
-    <option>Disable</option>
-  </select>
-  <label for="sel1" class="form-abel">Select list (select one):</label>
-</div>
-<div class="form-floating mb-3 mt-3">
-      <input type="text" class="form-control" id="email" placeholder="Enter email" name="email">
-      <label for="email">Title</label>
-    </div>
-	<div class="form-floating mb-3 mt-3">
-      <input type="text" class="form-control" id="email" placeholder="Enter email" name="email">
-      <label for="email">Link</label>
-    </div>
-	<div class="form-floating mb-3 mt-3">
-      <input type="text" class="form-control" id="email" placeholder="Enter email" name="email">
-      <label for="email">Target</label>
-    </div>
-	<div class="form-floating mb-3 mt-3">
-      <input type="text" class="form-control" id="email" placeholder="Enter email" name="email">
-      <label for="email">Email</label>
-    </div>
-	<div class="form-floating mb-3 mt-3">
-      <input type="text" class="form-control" id="email" placeholder="Enter email" name="email">
-      <label for="email">Icon</label>
-    </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
 
 
 <!-- End : Your Page Source -->
