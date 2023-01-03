f8ab94234e4222ee05268d042cf57659aae6ff02
imranfcc <121158622+imranfcc@users.noreply.github.com>
2023-01-03 23:07:37 +0500

    n

--- AdminUstad/pages/Plugin/Links/Pages/AddLink.php
+++ AdminUstad/pages/Plugin/Links/Pages/AddLink.php
@@ -45,9 +45,9 @@
 	</div>
 </main>
 <?php
-$con = mysqli_connect('localhost','imtest_new','12345');
-$insert = mysqli_query($con, "INSERT INTO sh_pl_links (Status, Title, Link, Target, InId, Icon)
-	VALUES ('Enable','Computer' ,'Laptop','Sale' ,'15')");
+
+$insert = mysqli_query("INSERT INTO sh_pl_links (Status, Title, Link, Target, InId, Icon)
+	VALUES ('Enable','Computer' ,'Laptop','Sale' ,15)");
 ?>	
 
 
