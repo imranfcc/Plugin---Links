32dcb166bf1a5390d5ae81c90aeba2641d631d09
imranfcc <121158622+imranfcc@users.noreply.github.com>
2023-01-03 22:57:16 +0500

    update

--- AdminUstad/pages/Plugin/Links/Pages/AddLink.php
+++ AdminUstad/pages/Plugin/Links/Pages/AddLink.php
@@ -48,7 +48,7 @@
 $con = mysqli_connect('localhost','imtest_new','12345');
 $insert = mysqli_query($con, "INSERT INTO sh_pl_links (Status, Title, Link, Target, InId, Icon)
 	VALUES ('Enable','Computer' ,'Laptop','Sale' ,'15')");
-	
+?>	
 
 
 <!-- End : Your Page Source -->
