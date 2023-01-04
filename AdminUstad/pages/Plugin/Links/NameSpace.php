4bd31c5bbee49138c6cc33469d810172a01bf4e6
imranfcc <121158622+imranfcc@users.noreply.github.com>
2023-01-04 22:01:10 +0500

    update namespace

--- AdminUstad/pages/Plugin/Links/NameSpace.php
+++ AdminUstad/pages/Plugin/Links/NameSpace.php
@@ -1,10 +1,30 @@
 <?php
 namespace P\Links;
 
+
+
 function lists($Get){
   global $con; 
   $sql = "SELECT * FROM `sh_pl_links`  WHERE `InId`='$Get[Id]'"; //
   $result = $con->query($sql);
   return $result;
 }
+function Id_to_Name($Id){
+  global $con;
+  $sql = "SELECT * FROM `sh_pl_links`  WHERE `Id`='$Id'"; //
+  $result = $con->query($sql);
+  $row = $result->fetch_assoc();
+  $return=$row["Title"];
+  return $return;
+
+}
+function Insert($Get){
+  global $con;
+  // Insert not complete
+  
+  $sql = "INSERT INTO `sh_pl_links` (`Status`, `Title`, `Link`)
+  VALUES ('Enable', 'Azhar', 'Links')";
+  $con->query($sql);
+}
+
 ?>
