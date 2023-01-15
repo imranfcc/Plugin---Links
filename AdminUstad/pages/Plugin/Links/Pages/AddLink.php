3302a8385bb1ef395dd2e17ba7ecb8d844bf7e5e
imranfcc <121158622+imranfcc@users.noreply.github.com>
2023-01-15 15:17:04 +0500

    update

--- AdminUstad/pages/Plugin/Links/Pages/AddLink.php
+++ AdminUstad/pages/Plugin/Links/Pages/AddLink.php
@@ -107,7 +107,7 @@ if(!isset($ValueInId)){ $ValueInId=$_GET["AutoFill_InId"]; }
 			<?php if(isset($_GET["EditLinksId"])){ ?>
 				<input name='EditLinksId' value="<?= $_GET["EditLinksId"];?>">
 			<?php } ?>
-  			<button type="submit" name="SubmitForm" value="Links" class="btn btn-lg  btn-primary w-100 - m">Submit</button>
+  			<button type="submit" name="SubmitForm" value="Links" class="btn btn-lg  btn-primary w-100 - m">Submit</button><br>
 			  <a class="btn btn-primary w-100" href="AdminUstad/Plugin/Links/pages/Delete.php?id=<?php echo $ValueInId; ?>" role="button">Delete-Record</a>
 			<!--<div class="d-grid gap-2">
   				<button class="btn btn-primary" type="submit" name='SubmitForm'>SubmitForm</button>
