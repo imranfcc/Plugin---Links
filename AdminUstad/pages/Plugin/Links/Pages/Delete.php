d27e4df742c30d375b3e81a4f886d5875fccd70d
imranfcc <121158622+imranfcc@users.noreply.github.com>
2023-01-15 22:41:52 +0500

    U

--- AdminUstad/pages/Plugin/Links/Pages/Delete.php
+++ AdminUstad/pages/Plugin/Links/Pages/Delete.php
@@ -5,7 +5,7 @@
         //if($con->query($sql)===TRUE){
         //header("Location: /".$AdminFolder."/Plugin/".$Plugin."/dashboard?ShowInId=".$ValueInId);
 
-    $sql = "DELETE FROM sh_pl_links WHERE InId = {$newid}";
-    $result = mysqli_query($con, $sql) or die ("connection fail");
+    $sql = "DELETE FROM sh_pl_links WHERE InId = {'$newid'}";
+    $result = mysqli_query($con,$sql) or die ("connection fail");
     
 ?>
