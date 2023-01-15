af4f4f0d96f7eb11d4d206583bb8198855e6a28e
imranfcc <121158622+imranfcc@users.noreply.github.com>
2023-01-15 22:37:14 +0500

    UP

--- AdminUstad/pages/Plugin/Links/Pages/Delete.php
+++ AdminUstad/pages/Plugin/Links/Pages/Delete.php
@@ -1,11 +1,11 @@
 <?php
 
-    include("pages/inc/Header.php"); 
-    $newid = $GET[id];
-    if($con->query($sql)===TRUE){
-    //header("Location: /".$AdminFolder."/Plugin/".$Plugin."/dashboard?ShowInId=".$ValueInId);
+    //include("pages/inc/Header.php"); 
+         $newid = $GET[id];
+        //if($con->query($sql)===TRUE){
+        //header("Location: /".$AdminFolder."/Plugin/".$Plugin."/dashboard?ShowInId=".$ValueInId);
 
     $sql = "DELETE FROM sh_pl_links WHERE InId = {$newid}";
     $result = mysqli_query($con, $sql) or die ("connection fail");
-    }
+    
 ?>
