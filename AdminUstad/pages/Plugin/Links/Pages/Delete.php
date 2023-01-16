3f4a5b332224113ac575287e41dc7a0945ef47eb
imranfcc <121158622+imranfcc@users.noreply.github.com>
2023-01-16 14:32:49 +0500

    check

--- AdminUstad/pages/Plugin/Links/Pages/Delete.php
+++ AdminUstad/pages/Plugin/Links/Pages/Delete.php
@@ -9,7 +9,7 @@
     $sql = "DELETE FROM `sh_pl_links` WHERE `Id`='$newid'";
     $result = mysqli_query($con,$sql) or die ("connection fail");
     header('Location: https://www.imrantest.sidhosting.net/AdminUstad/Plugin/Links/dashboard');
-    if($result){
+    if('$result'){
         echo"<script>alert('This is Deleted')</script>";
        }
        else{
